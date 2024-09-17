6508215dbb311bb68bdc58c22dbacc1c2789dc09
tejaspawar1503 <tejaspawar1503@gmail.com>
2024-09-17 13:56:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.getElementById('loading-screen').style.display = 'none';
-        document.getElementById('main-content').style.display = 'block';
-    }, 4000); // Fake loading for 4 seconds
-});
+window.addEventListener('scroll', reveal);
 
+function reveal() {
+    const reveals = document.querySelectorAll('.section-title, .skill, .project-card');
+
+    reveals.forEach(function (reveal) {
+        const windowHeight = window.innerHeight;
+        const revealTop = reveal.getBoundingClientRect().top;
+        const revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveal.classList.add('active');
+        } else {
+            reveal.classList.remove('active');
+        }
+    });
+}
